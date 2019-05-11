95386d17af08f9d107c785a2710d063e803baf99
igushev <igushev@gmail.com>
2019-05-10 23:39:52 -0700

    Updating deploy.py

--- deploy.py
+++ deploy.py
@@ -1,6 +1,6 @@
 import os
 
-from fase_lib.server_util import deploy_util
+from fase_lib.tools import deploy_util
 from fase_lib.tools import version_util
 
 
@@ -9,24 +9,22 @@ FASE_VERSION_FILENAME_VAR_NAME = 'FASE_VERSION_FILENAME'
 HOME_DIR_VAR_NAME = 'NOTES_FASE_DIR'
 VERSION_FILENAME_VAR_NAME = 'NOTES_FASE_VERSION_FILENAME'
 DEPLOY_DIR_VAR_NAME = 'NOTES_FASE_SERVER_DEPLOY_DIR'
-FILENAME_TEMPLATE = 'NotesFaseServer_Notes_%s_Fase_%s'
+DEPLOY_FILENAME_TEMPLATE = 'NotesFaseServer_Notes_%s_Fase_%s'
 
 def main(argv):
-  assert len(argv) <= 2
-  update_position = int(argv[1]) if len(argv) == 2 else None
   fase_lib_dir = os.environ[FASE_LIB_DIR_VAR_NAME]
   home_dir = os.environ[HOME_DIR_VAR_NAME]
   deploy_dir = os.environ[DEPLOY_DIR_VAR_NAME]
   assert deploy_dir, '%s must be set!' % DEPLOY_DIR_VAR_NAME
 
   version_filename = os.environ[VERSION_FILENAME_VAR_NAME]
-  version = version_util.ReadAndUpdateVersion(version_filename, update_position)
+  version = version_util.ReadAndUpdateVersion(version_filename)
   fase_version_filename = os.environ[FASE_VERSION_FILENAME_VAR_NAME]
   fase_version = version_util.ReadAndUpdateVersion(fase_version_filename)
 
-  filename = FILENAME_TEMPLATE % (version.replace('.', '_'), fase_version.replace('.', '_'))
+  deploy_filename = DEPLOY_FILENAME_TEMPLATE % (version.replace('.', '_'), fase_version.replace('.', '_'))
   copy_list = [(fase_lib_dir, os.path.basename(fase_lib_dir))]
-  deploy_util.Deploy(home_dir, copy_list, deploy_dir, filename)
+  deploy_util.Deploy(home_dir, copy_list, deploy_dir, deploy_filename)
 
 
 if __name__ == '__main__':
