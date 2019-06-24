6f9ac3ee6548cda810959978093cf845ecc92d17
igushev <igushev@gmail.com>
2019-06-24 00:51:10 -0700

    Fixing version_update.py

--- version_update.py
+++ version_update.py
@@ -3,13 +3,14 @@ import os
 from fase_lib.tools import version_util
 
 
-VERSION_FILENAME = 'version.txt'
+VERSION_FILENAME_VAR_NAME = 'NOTES_FASE_VERSION_FILENAME'
 
 
 def main(argv):
   assert len(argv) <= 2
   update_position = int(argv[1]) if len(argv) == 2 else None
-  version_util.ReadAndUpdateVersion(VERSION_FILENAME, update_position)
+  version_filename = os.environ[VERSION_FILENAME_VAR_NAME]
+  version_util.ReadAndUpdateVersion(version_filename, update_position)
 
 
 if __name__ == '__main__':
